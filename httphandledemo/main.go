c8065a71f7983b61cb539e85da29a07fd386285a
Kenshin <hi_appler@126.com>
2016-12-27 00:03:56 -0500

    http.HandleFunc

---
+++ httphandledemo/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func messageHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to Go Web Development")
+}
+func main() {
+	http.HandleFunc("/welcome", messageHandler)
+	log.Println("Listening...")
+	http.ListenAndServe(":9999", nil)
+}
