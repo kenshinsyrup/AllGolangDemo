4642537e2fda454ea07a4bfb68e052a08c067e71
Kenshin <hi_appler@126.com>
2017-04-11 00:29:03 -0400

    benchmark reflect.DeepEqual vs equal

--- CompareSlice/equal/equal.go
+++ CompareSlice/equal/equal.go
@@ -8,6 +8,13 @@ func StringSliceEqual(a, b []string) bool {
 	if len(a) != len(b) {
 		return false
 	}
+
+	// []int{} should not equal to []int(nil) i.e. var s []int or s := *new([]int)
+	// This keep consistent with reflect.DeepEqual
+	if (a == nil) != (b == nil) {
+		return false
+	}
+
 	for i := range a {
 		if a[i] != b[i] {
 			return false
