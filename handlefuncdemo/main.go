a856001f1fcb3302a674f01256dc939c3f22e12a
Kenshin <hi_appler@126.com>
2017-11-28 19:27:14 -0800

    验证: main进程在，goroutine不会被吃

--- handlefuncdemo/main.go
+++ handlefuncdemo/main.go
@@ -4,9 +4,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func messageHandler(w http.ResponseWriter, r *http.Request) {
+	go func() {
+		time.Sleep(time.Second * 2)
+		log.Println("hhahhaha 过一会儿再显示反正即使messageHandler return了， main进程还在，go routine不会被吃掉的")
+	}()
+	log.Println("我会出现在hahahha那一堆之前")
 	fmt.Fprintf(w, "Welcome to Go Web Development")
 }
 
@@ -15,5 +21,5 @@ func main() {
 	// Use the shortcut method ServeMux.HandleFunc
 	mux.HandleFunc("/welcome", messageHandler)
 	log.Println("Listening...")
-	http.ListenAndServe(":9999", mux)
+	http.ListenAndServe(":9998", mux)
 }
