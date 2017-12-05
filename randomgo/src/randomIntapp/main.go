405f26175204b6ad17b4fd99000301e5837ef995
Kenshin <hi_appler@126.com>
2017-12-04 18:59:11 -0800

    random intn

--- randomgo/src/randomIntapp/main.go
+++ randomgo/src/randomIntapp/main.go
@@ -6,9 +6,14 @@ import (
 	"time"
 )
 
+var random = rand.New(rand.NewSource(time.Now().UnixNano()))
+
 func main() {
-	fmt.Println(RandNum(10))
+	// fmt.Println(RandNum(10))
+
+	fmt.Println(random.Intn(30))
 }
-func RandNum(n int) int {
-	return rand.New(rand.NewSource(time.Now().UTC().UnixNano())).Intn(n)
-}
+
+// func RandNum(n int) int {
+// 	return rand.New(rand.NewSource(time.Now().UnixNano())).Intn(n)
+// }
